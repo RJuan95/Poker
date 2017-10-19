64335e2b80fb58b89f30e9464c17dc421fa3223b
Juan <jurosale@ucsc.edu>
2017-10-19 11:27:35 -0700

    part a-d complete

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -20,8 +22,8 @@ struct deck {
 
 struct hand {
 	int* array;
-	int capacity;
-	int count;
+	//int capacity;
+	//int count;
 };
 
 void first_card(deck*, int);
@@ -31,16 +33,20 @@ deck* create_inDeck();
 void add_inDeck(deck*, int);
 hand* create_hand();
 void play_game(deck*, int*);
+void draw_card(deck*, int);
+void remove_card_in_deck(int);
 void print(deck*);
-
+void print_hand(hand*);
 int* money;
 bool keepPlaying;
 deck* Deck;
 deck* InDeck;
+hand* Hand;
 string input;
 
 int main()
 {
+	srand(time(NULL));
 	int a = 10;
 	money = &a;
 	keepPlaying = true;
@@ -48,8 +54,9 @@ int main()
 	for (int i = 0; i < 52; i++) { new_card(Deck, i); }
 	
 	InDeck = create_inDeck();
-	hand* Hand = create_hand();
+	Hand = create_hand();
 	
+	for (int i = 0; i < 52; i++) { add_inDeck(Deck, i); }
 	while (*money > 0 && keepPlaying == true) {
 		play_game(Deck, &a);
 	}
@@ -130,7 +137,7 @@ deck* create_inDeck() {
 void add_inDeck(deck* currDeck, int index) {
 	card* temp = currDeck->head;
 	for (int i = 0; i < index; i++) { temp = temp->next; }
-	if (temp->state != "deck") { return; }
+	//if (temp->state != "deck") { return; }
 	if (InDeck->count == InDeck->capacity) {
 		int new_capacity = InDeck->capacity * 2;
 		int* new_array = new int[new_capacity];
@@ -147,20 +154,43 @@ hand* create_hand() {
 	const int INIT_CAP = 5;
 	hand* list = new hand;
 	list->array = new int[INIT_CAP];
-	list->capacity = INIT_CAP;
-	list->count = 0;
+	//list->capacity = INIT_CAP;
+	//list->count = 0;
 	return list;
 }
 
 void play_game(deck* currDeck, int* currMoney) {
-	*currMoney = *currMoney - 10;
+	*currMoney = *currMoney - 1;
+	input = "none";
 	cout << "You currently have $" << *currMoney << " after paying $1 to play this round." << endl;
-	for (int i = 0; i < 52; i++) { add_inDeck(currDeck, i); }
-	cout << endl << "type 'deck' to see the deck." << endl << "Your response : ";
-	while (input != "deck") {
+	cout << endl << "type 'deck' to see the deck or 'play' to start a new round." << endl << "Your response : ";
+	while (input == "none") {
 		getline(cin, input);
+		if (input != "deck" && input != "play") { 
+			cout << "Sorry that is not a valid response. Try again." << endl << "Your response: ";
+			input = "none"; 
+		}
 	}
 	if (input == "deck") { print(InDeck); }
+	else if (input == "play") {
+		for (int i = 0; i < 5; i++) { draw_card(InDeck, i); }
+		print_hand(Hand);
+	}
+}
+
+void draw_card(deck* thisDeck, int handIndex) {
+	int index = rand() % InDeck->count;
+	Hand->array[handIndex] = InDeck->array[index];
+	remove_card_in_deck(index);
+}
+
+void remove_card_in_deck(int index){
+	if (index < 0 || index >= InDeck->count) {
+		cout << "Error! out of bound" << endl;
+		return;
+	}
+	for (int i = index; i < InDeck->count - 1; i++) { InDeck->array[i] = InDeck->array[i + 1]; }
+	InDeck->count--;
 }
 
 void print(deck* thisDeck) {
@@ -190,5 +220,48 @@ void print(deck* thisDeck) {
 			}
 		}
 	temp = temp->next;
+	}
+}
+
+void print_hand(hand* currHand) {
+	cout << "Your Hand:" << endl;
+	for (int i = 0; i < 5; i++) {
+		card* temp = Deck->head;
+		for (int j = 0; j < currHand->array[i]; j++) { temp = temp->next; }
+		switch (i) {
+		case 0:
+			cout << "A: ";
+			break;
+		case 1:
+			cout << "B: ";
+			break;
+		case 2:
+			cout << "C: ";
+			break;
+		case 3:
+			cout << "D: ";
+			break;
+		case 4:
+			cout << "E: ";
+			break;
+		}
+		switch (temp->value) {
+		case 1:
+			cout << "Ace of " << temp->suit << endl;
+			break;
+		case 11:
+			cout << "Jack of " << temp->suit << endl;
+			break;
+		case 12:
+			cout << "Queen of " << temp->suit << endl;
+			break;
+		case 13:
+			cout << "King of " << temp->suit << endl;
+			break;
+		default:
+			cout << temp->value << " of " << temp->suit << endl;
+			break;
+		}
+
 	}
 }
